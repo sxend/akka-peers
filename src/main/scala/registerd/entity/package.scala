8bcf57ca340f0ede5a1c6d10b603c0b30f140cc2
sxend <arimitsu.k+github@gmail.com>
2017-04-24 12:12:48 +0900

    fix config

--- src/main/scala/registerd/entity/package.scala
+++ src/main/scala/registerd/entity/package.scala
@@ -5,22 +5,21 @@ import org.apache.commons.codec.digest.DigestUtils
 package object entity {
 
   implicit class BlockOps(block: Block) {
-    lazy val digest: String = hash(block.toByteArray)
+    lazy val digest: Array[Byte] = hash(block.toByteArray)
   }
   implicit class PayloadOps(payload: Payload) {
-    lazy val digest: String = hash(payload.toByteArray)
+    lazy val digest: Array[Byte] = hash(payload.toByteArray)
   }
   implicit class PayloadsOps(payloads: List[Payload]) {
-    lazy val digest: String = flatten(payloads.map(_.digest): _*)
+    lazy val digest: Array[Byte] = flatten(payloads.map(_.digest): _*)
 
-    private def flatten(digests: String*): String = digests match {
-      case a :: Nil       => hash(a + a)
-      case a :: b :: Nil  => hash(a + b)
-      case a :: b :: tail => hash(flatten(a, b) + flatten(tail: _*))
+    private def flatten(digests: Array[Byte]*): Array[Byte] = digests match {
+      case a :: Nil       => hash(a ++ a)
+      case a :: b :: Nil  => hash(a ++ b)
+      case a :: b :: tail => hash(flatten(a, b) ++ flatten(tail: _*))
     }
   }
 
-  private def hash(data: Array[Byte]): String = DigestUtils.sha256Hex(data)
-  private def hash(data: String): String = DigestUtils.sha256Hex(data)
+  private def hash(data: Array[Byte]): Array[Byte] = DigestUtils.sha256(data)
 
 }
